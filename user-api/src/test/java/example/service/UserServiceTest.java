64f9394c5313131da80d9f4d12f6aabce7f326bd
k-mong <rlaghdwns777@naver.com>
2024-03-21 17:24:07 +0900

    Feat:오류수정 및 UserService추가

---
+++ user-api/src/test/java/example/service/UserServiceTest.java
@@ -0,0 +1,15 @@
+package example.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class UserServiceTest {
+    @Test
+    void 회원가입_테스트() {
+
+
+    }
+
+}
