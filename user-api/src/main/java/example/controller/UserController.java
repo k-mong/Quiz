980f6ab87182c5b88571ae1351f66170e72b08e1
k-mong <rlaghdwns777@naver.com>
2024-04-01 23:18:57 +0900

    Feat:filter, HTML, controller추가

---
+++ user-api/src/main/java/example/controller/UserController.java
@@ -0,0 +1,18 @@
+package example.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    @PostMapping("/test/login")
+    public String test() {
+        return "들어옴";
+    }
+}
